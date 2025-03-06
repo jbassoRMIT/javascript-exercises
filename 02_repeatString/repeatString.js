db7a0eacd5727165e0b340e365a35bce60b8630f
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-03-07 07:02:31 +0900

    02 - repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let returnWord = "";
+    if (num < 0) {
+        return "ERROR"
+    }
+    else {
+        for (let i = 0; i < num; i++) {
+            returnWord += string;
+        }
+        return returnWord;
+    }
 
 };
 
